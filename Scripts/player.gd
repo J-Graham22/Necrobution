c88ff6bfe9e13e8fc8eb7e6720907d449c93d9fd
Justin Graham <justin.m.graham2@gmail.com>
2026-03-05 22:36:24 -0500

    initial tile based movement in overworld

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,22 +4,38 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@export var tile_size: int = 32
+@export var speed: int = 200
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+var moving: bool = false
+var target_pos: Vector2
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _ready() -> void:
+	target_pos = global_position
+	
+func _process(delta: float) -> void:
+	if moving:
+		global_position = global_position.move_toward(target_pos, speed * delta)
+		
+		if global_position == target_pos:
+			moving = false
+			
+		return
+		
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_just_pressed("ui_left"):
+		direction = Vector2.UP
+	elif Input.is_action_just_pressed("ui_right"):
+		direction = Vector2.RIGHT
+	elif Input.is_action_just_pressed("ui_left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_just_pressed("ui_down"):
+		direction = Vector2.DOWN
+	
+	if direction != Vector2.ZERO:
+		move(direction)
+		
+func move(direction: Vector2) -> void:
+	target_pos = global_position + direction * tile_size
+	moving = true
